5ce6f4fa38f0472192c4c69349e246d8c30b5ca5
dembasow98 <dastech1998@gmail.com>
2023-02-12 20:13:51 +0300

    Don't with implementing the app.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import './App.css';
+import React, { useState } from 'react';
 
 import counterStore from './store';
 
 function App() {
 
+  const [counter, setCounter] = useState(0);
+
   //Increment the counter by 1
   const increment = () => {
-    counterStore.dispatch({type: 'INCREMENT', payload: 1});
+    counterStore.dispatch({type: 'INCREMENT'});
+    setCounter(counterStore.getState().count);
   }
 
   //Decrement the counter by 1
   const decrement = () => {
-    counterStore.dispatch({type: 'DECREMENT', payload: 1});
+    counterStore.dispatch({type: 'DECREMENT'});
+    setCounter(counterStore.getState().count);
   }
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Counter React Redux App
-        </p>
-        <div className="App-body">
-        <div className="App-body-counter">
-          <p>Counter:{counterStore.getState().count}</p>
+    <div className="flex flex-col w-full h-screen dark:bg-gray-900 dark:text-white items-center">
+      <div className="flex flex-col w-full h-full items-center justify-center dark:bg-gray-800 m-36 p-10 max-w-xl">
+        <div className="flex flex-col w-full h-full items-center justify-center">
+          <h1 className="text-4xl font-bold">Counter</h1>
+          <div className="flex flex-row w-full h-full items-center justify-center">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-l" onClick={decrement}>-</button>
+            <div className="bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-gray-200 font-bold py-2 px-4">
+              {counter}
+            </div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r" onClick={increment}>+</button>
+          </div>
         </div>
-        <div className="App-body-buttons">
-          <button onClick={decrement}>Decrement</button>
-          <button onClick={increment}>Increment</button>
-        </div>
-        </div>
-      </header>
-    
+      </div>
     </div>
   );
 }
